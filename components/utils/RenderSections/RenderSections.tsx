3baffd2a14d4780bef9cb2a7b3e78acbfd369d56
Rob Easthope <hello@robeasthope.com>
2021-08-13 15:01:07 +0100

    Copy in Render sections util

---
+++ components/utils/RenderSections/RenderSections.tsx
@@ -0,0 +1,62 @@
+import * as SectionComponents from 'components/utils/RenderSections/section-index';
+
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { upperFirst } from 'lodash';
+
+function resolveSections(section) {
+  // eslint-disable-next-line import/namespace
+  const Section = SectionComponents[upperFirst(section._type)];
+
+  if (Section) {
+    return Section;
+  }
+
+  console.error('Cant find section', section); // eslint-disable-line no-console
+  return null;
+}
+
+export function RenderSections({
+  sections,
+  prefilledSections,
+  features,
+  settings,
+  locale,
+  preview,
+  overlayNav,
+}) {
+  if (!sections) {
+    console.error('Missing section');
+    return <div>Missing sections</div>;
+  }
+
+  return (
+    <>
+      {sections?.map((section) => {
+        const SectionComponent = resolveSections(section);
+
+        if (!SectionComponent) {
+          return (
+            <div key={section?._key}>Missing section {section?._type}</div>
+          );
+        }
+
+        return (
+          <SectionComponent
+            // eslint-disable-next-line react/jsx-props-no-spreading
+            {...section}
+            key={`render-sections-${section._key as string}`}
+            prefilledSections={prefilledSections}
+            features={features}
+            settings={settings}
+            locale={locale}
+            preview={preview}
+            overlayNav={overlayNav}
+          />
+        );
+      })}
+    </>
+  );
+}
