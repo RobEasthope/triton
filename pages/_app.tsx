c88e7c0254d1f7aabf4e8f578445168f572405bb
Rob Easthope <hello@robeasthope.com>
2021-08-13 09:27:58 +0100

    Integrate ChakraProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
 import type { AppProps } from 'next/app';
 import { ReactNode } from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
 
 function MyApp({ Component, pageProps }: AppProps): ReactNode {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
+
 export default MyApp;
