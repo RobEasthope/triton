2fab3b9b3b4e91c89b29d118f3f461ca4d1a49a1
Rob Easthope <hello@robeasthope.com>
2021-08-13 11:57:27 +0100

    Rename repo

--- components/layouts/PageLayout.tsx
+++ components/layouts/PageLayout.tsx
@@ -2,7 +2,7 @@ import { Heading } from '../typography/Heading/Heading';
 
 export const PageLayout = () => (
   <div className="">
-    <h1 className="">Welcome to Triquetra</h1>
+    <h1 className="">Welcome to Triton</h1>
     <Heading as="h1" className="">
       Heading
     </Heading>
