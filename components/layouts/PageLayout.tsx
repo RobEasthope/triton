a5b3a44ee4e65e618670046678cc43b2f92726fc
Rob Easthope <hello@robeasthope.com>
2021-08-13 10:30:16 +0100

    Test Heading

--- components/layouts/PageLayout.tsx
+++ components/layouts/PageLayout.tsx
@@ -1,5 +1,10 @@
+import { Heading } from '../typography/Heading/Heading';
+
 export const PageLayout = () => (
   <div className="">
     <h1 className="">Welcome to Triquetra</h1>
+    <Heading as="h1" className="">
+      Heading
+    </Heading>
   </div>
 );
