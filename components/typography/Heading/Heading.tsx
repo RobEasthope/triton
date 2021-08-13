3c1b7155e1beba1c6a3ba7d97a74025ddce0aecd
Rob Easthope <hello@robeasthope.com>
2021-08-13 10:57:36 +0100

    Restrict Heading 'as' prop typings to 'hX' values

--- components/typography/Heading/Heading.tsx
+++ components/typography/Heading/Heading.tsx
@@ -1,6 +1,8 @@
 import { Heading as ChakraHeading } from '@chakra-ui/react';
 import styled from '@emotion/styled';
 
-export const Heading = styled(ChakraHeading)<{ as: string }>`
+export const Heading = styled(ChakraHeading)<{
+  as: 'h1' | 'h2' | 'h2' | 'h4' | 'h5' | 'h6';
+}>`
   color: turquoise;
 `;
