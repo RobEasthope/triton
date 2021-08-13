008477faf97b77748049778902c45e9ce179efff
Rob Easthope <hello@robeasthope.com>
2021-08-13 12:21:48 +0100

    Add in absolute paths support in storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,5 +6,13 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials"
-  ]
+  ],
+  webpackFinal: async (config, { configType }) => {
+    config.resolve.modules = [path.resolve(__dirname, ".."), "node_modules"];
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname, "../"),
+    };
+    return config;
+  },
 }
