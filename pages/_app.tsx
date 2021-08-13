1365fc61cee1abd7e56aae7aa25c74204072e8d2
Rob Easthope <hello@robeasthope.com>
2021-08-13 10:21:12 +0100

    Integrate globalStyles styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,12 @@ import type { AppProps } from 'next/app';
 import { ReactNode } from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
 import { theme } from '../theme/theme';
+import { globalStyles } from '../theme/globalStyles';
 
 function MyApp({ Component, pageProps }: AppProps): ReactNode {
   return (
     <ChakraProvider theme={theme}>
+      {globalStyles}
       <Component {...pageProps} />
     </ChakraProvider>
   );
