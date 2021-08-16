15a0cb67c7b5df08adcdc8d982274b98b9435643
Rob Easthope <hello@robeasthope.com>
2021-08-16 11:47:56 +0100

    Update title placeholder name

--- pages/[[...slug]].tsx
+++ pages/[[...slug]].tsx
@@ -42,7 +42,7 @@ export default function PageBySlug({
   return (
     <>
       <Head>
-        <title>Triquetra</title>
+        <title>Triton</title>
       </Head>
 
       {isFallback && <Loading />}
