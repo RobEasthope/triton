7e293687dc38890490da96c26c15de8d953de0b7
Rob Easthope <hello@robeasthope.com>
2021-08-16 20:17:02 +0100

    Hardcode sanity id's

--- utils/sanity/config.ts
+++ utils/sanity/config.ts
@@ -1,8 +1,6 @@
 export const sanityConfig = {
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
-  projectId:
-    process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ||
-    process.env.STORYBOOK_SANITY_PROJECT_ID,
+  dataset: 'production',
+  projectId: 'xtxdr2ns',
   token: process.env.SANITY_API_TOKEN,
   useCdn: true,
   withCredentials: true,
