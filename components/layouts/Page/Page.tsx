c7c3c731d382da19c8d9081f5ef8085cdd306c3a
Rob Easthope <hello@robeasthope.com>
2021-08-16 16:46:35 +0100

    Temp fix for TS issues

--- components/layouts/Page/Page.tsx
+++ components/layouts/Page/Page.tsx
@@ -1,6 +1,15 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Heading } from 'components/typography/Heading/Heading';
 
-export const Page = () => (
+export const Page = ({
+  page,
+  preview,
+}: {
+  page: Record<string, unknown>;
+  preview: boolean;
+}) => (
   <div className="">
     <h1 className="">Welcome to Triton</h1>
     <Heading as="h1" className="">
