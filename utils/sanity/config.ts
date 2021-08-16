bd33ab8bcbfb15c86eba245d3a6428ade0b4494b
Rob Easthope <hello@robeasthope.com>
2021-08-16 16:59:47 +0100

    Clean up sanityConfig

--- utils/sanity/config.ts
+++ utils/sanity/config.ts
@@ -1,10 +1,8 @@
 export const sanityConfig = {
-  // Find your project ID and dataset in `sanity.json` in your studio project
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
   projectId:
     process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ||
     process.env.STORYBOOK_SANITY_PROJECT_ID,
-  // useCdn: process.env.NODE_ENV === 'production',
   token: process.env.SANITY_API_TOKEN,
   useCdn: true,
   withCredentials: true,
