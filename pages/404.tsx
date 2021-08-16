fc9ee5e256261c8d2966be3241b8b9f544cabc28
Rob Easthope <hello@robeasthope.com>
2021-08-16 11:47:13 +0100

    Convert 404 page to functional component

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,5 +1,7 @@
-export const Error404 = () => (
-  <div>
-    <h1>Error 404</h1>
-  </div>
-);
+export default function Custom404() {
+  return (
+    <div>
+      <h1>Error 404</h1>
+    </div>
+  );
+}
