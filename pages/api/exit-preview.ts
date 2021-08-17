1f6ecdb740d0d3f8d2c2b90c5d094f825c78f64e
Rob Easthope <hello@robeasthope.com>
2021-08-17 11:08:45 +0100

    Remove _ prop from exite-preview lambda

--- pages/api/exit-preview.ts
+++ pages/api/exit-preview.ts
@@ -1,7 +1,7 @@
 import type { NextApiResponse } from 'next';
 
 // eslint-disable-next-line @typescript-eslint/require-await
-export default async function exit(_, res: NextApiResponse): Promise<void> {
+export default async function exit(res: NextApiResponse): Promise<void> {
   // Exit the current user from "Preview Mode". This function accepts no args.
   res.clearPreviewData();
 
