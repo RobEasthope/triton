02f0061d59788e83ba4bcfde0ad6c6d13993f32e
Rob Easthope <hello@robeasthope.com>
2021-08-17 17:53:17 +0100

    Add types for ExternalLink

--- components/navigation/raw/ExternalLink/ExternalLink.tsx
+++ components/navigation/raw/ExternalLink/ExternalLink.tsx
@@ -1,4 +1,21 @@
-export const ExternalLink = ({ link, onClick, children, className }) => (
+import {
+  ExternalLinkSansTitleProp,
+  ExternalLinkWithTitleProp,
+} from 'types/links';
+
+export type ExternalLinkProps = {
+  link: ExternalLinkWithTitleProp | ExternalLinkSansTitleProp;
+  className?: string;
+  children: unknown;
+  onClick?: VoidFunction;
+};
+
+export const ExternalLink = ({
+  link,
+  onClick,
+  children,
+  className,
+}: ExternalLinkProps) => (
   <a
     href={link.url}
     target="_blank"
