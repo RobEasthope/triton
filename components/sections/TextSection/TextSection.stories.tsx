a8038f45b4351814a3a268fb75eae7606b460ff0
Rob Easthope <hello@robeasthope.com>
2021-08-17 14:40:30 +0100

    Draft out TextSection story

---
+++ components/sections/TextSection/TextSection.stories.tsx
@@ -0,0 +1,40 @@
+// eslint-disable-next-line no-use-before-define
+import React from 'react';
+import faker from 'faker';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Meta, Story } from '@storybook/react';
+import { TextSection as TextSectionProps } from 'types/sanity-schema';
+import { capitalizeFirstLetter } from 'utils/storybook/capitalizeFirstLetter';
+
+import { TextSection } from './TextSection';
+import { CenterComponent } from '.storybook/preview-ui/CenterComponent';
+
+export default {
+  title: 'Sections/Text',
+  component: TextSection,
+  decorators: [
+    // eslint-disable-next-line @typescript-eslint/no-shadow, no-shadow
+    (Story) => (
+      <CenterComponent>
+        <Story />
+      </CenterComponent>
+    ),
+  ],
+  parameters: {
+    badges: ['dev'],
+  },
+} as Meta;
+
+const Template: Story<TextSectionProps> = (args) => <TextSection {...args} />;
+
+// Primary
+export const Primary = Template.bind({}) as Record<string, unknown>;
+
+Primary.args = {
+  heading: capitalizeFirstLetter(faker.lorem.words()),
+  text: capitalizeFirstLetter(faker.lorem.words()),
+};
+
+// No data
+export const NoData = Template.bind({}) as Record<string, unknown>;
+NoData.args = {};
