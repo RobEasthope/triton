0bfe1e08499144ef41afdc450b748093b12d6b33
Rob Easthope <hello@robeasthope.com>
2021-08-17 16:50:35 +0100

    Add globalStorybookStyles to Storybook preview

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,7 +1,18 @@
+import React from 'react';
 import { addDecorator } from '@storybook/react';
 import { withPerformance } from 'storybook-addon-performance';
+import { globalStorybookStyles } from './preview-ui/globalStorybookStyles';
 
 addDecorator(withPerformance);
+
+export const decorators = [
+  (Story) => (
+    <>
+      {globalStorybookStyles}
+      <Story />
+    </>
+  ),
+];
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
