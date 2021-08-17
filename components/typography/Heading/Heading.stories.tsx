f098888d98ff0dc06a3ddfd593d653f7825d94c0
Rob Easthope <hello@robeasthope.com>
2021-08-17 16:01:44 +0100

    Draft out Heading story

---
+++ components/typography/Heading/Heading.stories.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import faker from 'faker';
+import { Meta, Story } from '@storybook/react';
+
+import { CenterTextComponent } from '.storybook/preview-ui/CenterTextComponent';
+import { Heading } from './Heading';
+
+export default {
+  title: 'Typography/Headings',
+  component: Heading,
+  parameters: {
+    badges: ['dev'],
+  },
+  decorators: [
+    // eslint-disable-next-line @typescript-eslint/no-shadow, no-shadow
+    (Story) => (
+      <CenterTextComponent>
+        <Story />
+      </CenterTextComponent>
+    ),
+  ],
+} as Meta;
+
+const Template: Story = (args) => <Heading {...args} />;
+
+// H1 (Bold)
+export const Heading1 = Template.bind({}) as Record<string, unknown>;
+Heading1.args = {
+  as: 'h1',
+  className: 'h1',
+  children: faker.lorem.words(),
+};
+
+// H2 (Bold)
+export const Heading2 = Template.bind({}) as Record<string, unknown>;
+Heading2.args = {
+  as: 'h2',
+  className: 'h2',
+  children: faker.lorem.words(),
+};
