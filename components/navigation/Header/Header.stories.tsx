54f9034dae859cd3fb56919872f989acd9c7268c
Rob Easthope <hello@robeasthope.com>
2021-08-18 14:42:07 +0100

    Draft out Header story

---
+++ components/navigation/Header/Header.stories.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import faker from 'faker';
+import { Meta, Story } from '@storybook/react';
+import { Header as HeaderProps } from 'types/sanity-schema';
+import { capitalizeFirstLetter } from '.storybook/utils/capitalizeFirstLetter';
+
+import { Header } from './Header';
+import { CenterComponent } from '.storybook/preview-ui/CenterComponent';
+
+export default {
+  title: 'Sections/Example',
+  component: Header,
+  decorators: [
+    // eslint-disable-next-line @typescript-eslint/no-shadow, no-shadow
+    (Story) => (
+      <CenterComponent>
+        <Story />
+      </CenterComponent>
+    ),
+  ],
+} as Meta;
+
+const Template: Story<HeaderProps> = (args) => <Header {...args} />;
+
+// Primary
+export const Primary = Template.bind({}) as Record<string, unknown>;
+
+Primary.args = {
+  navigation: capitalizeFirstLetter(faker.lorem.words()),
+};
+
+// No data
+export const NoData = Template.bind({}) as Record<string, unknown>;
+NoData.args = {};
