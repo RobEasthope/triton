5b88f2de89af3f590fc1639e4cdc86273538eb60
Rob Easthope <hello@robeasthope.com>
2021-08-18 16:59:45 +0100

    Draft out ExampleText markup

---
+++ components/formatted-text/ExampleText/ExampleText.tsx
@@ -0,0 +1,64 @@
+/* eslint-disable react/display-name */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+import BlockContent from '@sanity/block-content-to-react';
+
+import { Heading } from 'components/typography/Heading/Heading';
+// import { YoutubeBlock } from 'components/blocks/YoutubeBlock/YoutubeBlock';
+
+export interface ExampleTextProps {
+  blocks: unknown;
+  className?: string;
+  as?: string;
+  preview?: boolean;
+}
+
+const BlockRenderer = (props) => {
+  const { style = 'normal' } = props?.node as Record<string, unknown>;
+
+  if (style === 'h2') {
+    return <Heading as="h2">{props.children}</Heading>;
+  }
+  if (style === 'h3') {
+    return <Heading as="h3">{props.children}</Heading>;
+  }
+
+  if (style === 'h4') {
+    return <Heading as="h4">{props.children}</Heading>;
+  }
+
+  // Fall back to default handling
+  return BlockContent.defaultSerializers.types.block(props);
+};
+
+const serializers = {
+  types: {
+    block: BlockRenderer,
+
+    // Youtube block
+    // youtubeBlock: (props) => {
+    //   // eslint-disable-next-line react/destructuring-assignment, @typescript-eslint/naming-convention
+    //   const { _type, url } = props?.node;
+
+    //   return <YoutubeBlock _type={_type} url={url} />;
+    // },
+  },
+};
+
+export const ExampleText = ({
+  blocks,
+  className,
+  as,
+  preview,
+}: PortableTextProps) => (
+  <div className={className}>
+    <BlockContent
+      as={as}
+      blocks={blocks}
+      serializers={serializers}
+      preview={preview}
+    />
+  </div>
+);
