1408d844b75b1ad0477e34e34272cd9841dbf00a
Rob Easthope <hello@robeasthope.com>
2021-08-18 17:00:00 +0100

    Update example text heading types

--- components/formatted-text/ExampleText/example-text-schema.js
+++ components/formatted-text/ExampleText/example-text-schema.js
@@ -7,10 +7,9 @@ export default {
       type: 'block',
       styles: [
         { title: 'Normal', value: 'normal' },
-        { title: 'H1', value: 'h1' },
-        { title: 'H2', value: 'h2' },
-        { title: 'H3', value: 'h3' },
-        { title: 'H4', value: 'h4' },
+        { title: 'Large heading', value: 'h2' },
+        { title: 'Medium heading', value: 'h3' },
+        { title: 'Small heading', value: 'h4' },
       ],
       marks: {
         decorators: [
