762943ec2c6eeac92f61668fbad8ce1ca0f50143
Rob Easthope <hello@robeasthope.com>
2021-08-18 17:34:29 +0100

    Draft out example text story

---
+++ components/formatted-text/ExampleText/ExampleText.stories.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import faker from 'faker';
+import { Meta, Story } from '@storybook/react';
+import { capitalizeFirstLetter } from '.storybook/utils/capitalizeFirstLetter';
+
+import { ExampleText, ExampleTextProps } from './ExampleText';
+import { CenterComponent } from '.storybook/preview-ui/CenterComponent';
+
+export default {
+  title: 'Sections/Example',
+  component: ExampleText,
+  decorators: [
+    // eslint-disable-next-line @typescript-eslint/no-shadow, no-shadow
+    (Story) => (
+      <CenterComponent>
+        <Story />
+      </CenterComponent>
+    ),
+  ],
+} as Meta;
+
+const Template: Story<ExampleTextProps> = (args) => (
+  <ExampleText {...args} preview={false} />
+);
+
+// Primary
+export const Primary = Template.bind({}) as Record<string, unknown>;
+
+Primary.args = {
+  blocks: {},
+};
+
+// No data
+export const NoData = Template.bind({}) as Record<string, unknown>;
+NoData.args = {};
