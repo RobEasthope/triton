6b32c6d1e834df7132fe5600fec4a22943852341
Rob Easthope <hello@robeasthope.com>
2021-08-18 16:51:43 +0100

    Draft out example text schema

---
+++ components/formatted-text/ExampleText/example-text-schema.js
@@ -0,0 +1,29 @@
+export default {
+  title: 'Example text',
+  name: 'exampleText',
+  type: 'array',
+  of: [
+    {
+      type: 'block',
+      styles: [
+        { title: 'Normal', value: 'normal' },
+        { title: 'H1', value: 'h1' },
+        { title: 'H2', value: 'h2' },
+        { title: 'H3', value: 'h3' },
+        { title: 'H4', value: 'h4' },
+      ],
+      marks: {
+        decorators: [
+          { title: 'Strong', value: 'strong' },
+          { title: 'Emphasis', value: 'em' },
+          { title: 'Code', value: 'code' },
+        ],
+        annotations: [
+          { type: 'externalLinkBlock' },
+          { type: 'internalLinkBlock' },
+        ],
+      },
+    },
+    // { type: 'youtubeBlock' },
+  ],
+};
