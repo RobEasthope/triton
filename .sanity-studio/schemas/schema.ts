e861829065c973b21ab1055ae110ccb7c2638e0f
Rob Easthope <hello@robeasthope.com>
2021-08-19 11:31:27 +0100

    Add exampleText to schema index

--- .sanity-studio/schemas/schema.ts
+++ .sanity-studio/schemas/schema.ts
@@ -4,6 +4,9 @@ import createSchema from 'part:@sanity/base/schema-creator';
 // Then import schema types from any plugins that might expose them
 import schemaTypes from 'all:part:@sanity/base/schema-type';
 
+// Formatted text
+import exampleText from '../../components/utils/formatted-text/ExampleText/example-text-schema';
+
 // Sections
 import exampleSection from '../../components/sections/ExampleSection/example-section-schema';
 
@@ -18,6 +21,9 @@ export default createSchema({
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
+    // Formatted text
+    exampleText,
+
     // Sections
     exampleSection,
 
