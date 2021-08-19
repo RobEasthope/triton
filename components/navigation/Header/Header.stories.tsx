b9bff08a983db22b3fbd830cdb4fd488e6a0fe03
Rob Easthope <hello@robeasthope.com>
2021-08-19 14:00:01 +0100

    Solid data for header story

--- components/navigation/Header/Header.stories.tsx
+++ components/navigation/Header/Header.stories.tsx
@@ -26,7 +26,43 @@ const Template: Story<HeaderProps> = (args) => <Header {...args} />;
 export const Primary = Template.bind({}) as Record<string, unknown>;
 
 Primary.args = {
-  navigation: capitalizeFirstLetter(faker.lorem.words()),
+  logo: {
+    _type: 'image',
+    asset: {
+      _ref: 'image-b89231c30f9483550700246b4153e268b9235997-352x664-svg',
+      _type: 'reference',
+    },
+  },
+  navigation: [
+    {
+      _key: '7c4bc9de2735',
+      _type: 'internalLinkWithTitle',
+      title: 'Home',
+      to: {
+        slug: {
+          _type: 'slug',
+          current: 'root',
+        },
+      },
+    },
+    {
+      _key: '10514eca5a00',
+      _type: 'internalLinkWithTitle',
+      title: 'About',
+      to: {
+        slug: {
+          _type: 'slug',
+          current: 'about',
+        },
+      },
+    },
+    {
+      _key: 'a263b6d3ac5c',
+      _type: 'externalLinkWithTitle',
+      title: 'External',
+      url: 'https://placeholder.com',
+    },
+  ],
 };
 
 // No data
