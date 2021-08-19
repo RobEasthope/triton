bc9d25ce283e103fc219d549847f00a90f3d4bd2
Rob Easthope <hello@robeasthope.com>
2021-08-19 15:38:10 +0100

    Remove testing markup

--- components/layouts/HomePage/HomePage.tsx
+++ components/layouts/HomePage/HomePage.tsx
@@ -1,13 +1,7 @@
-import { Heading } from 'components/typography/Heading/Heading';
 import { RenderSections } from 'components/utils/RenderSections/RenderSections';
 
 export const HomePage = ({ page, preview }) => (
   <div className="">
-    <h1 className="">Welcome to Triton</h1>
-    <Heading as="h1" className="">
-      Heading
-    </Heading>
-
     <main className="">
       {page?.sections && (
         <RenderSections sections={page?.sections} preview={preview} />
