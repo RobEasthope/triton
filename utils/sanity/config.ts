80c270ffb9829f8a8c206d066c362f3ebcc8eb15
Rob Easthope <hello@robeasthope.com>
2021-08-19 15:15:32 +0100

    Set API version statically with todays date

--- utils/sanity/config.ts
+++ utils/sanity/config.ts
@@ -1,6 +1,7 @@
 export const sanityConfig = {
   dataset: 'production',
   projectId: 'xtxdr2ns',
+  apiVersion: '2021-08-19',
   token: process.env.SANITY_API_TOKEN,
   useCdn: true,
   withCredentials: true,
