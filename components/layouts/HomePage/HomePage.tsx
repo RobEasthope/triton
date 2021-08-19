f9cc191a1daa7bc8835811d14085f7c2d0be38dd
Rob Easthope <hello@robeasthope.com>
2021-08-19 15:36:39 +0100

    Set HomePage props

--- components/layouts/HomePage/HomePage.tsx
+++ components/layouts/HomePage/HomePage.tsx
@@ -1,7 +1,7 @@
 import { Heading } from 'components/typography/Heading/Heading';
 import { RenderSections } from 'components/utils/RenderSections/RenderSections';
 
-export const HomePage = () => (
+export const HomePage = ({ page, preview }) => (
   <div className="">
     <h1 className="">Welcome to Triton</h1>
     <Heading as="h1" className="">
