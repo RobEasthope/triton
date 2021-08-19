74c8f1fd095e8e02b095da8eb0a81db138018f05
Rob Easthope <hello@robeasthope.com>
2021-08-19 18:02:12 +0100

    Fix duplicate imports

--- components/layouts/HomePage/HomePage.tsx
+++ components/layouts/HomePage/HomePage.tsx
@@ -2,13 +2,12 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable react/prop-types */
 import { Footer } from 'components/navigation/Footer/Footer';
-import { Header } from 'components/navigation/Header/Header';
+import { Header, HeaderProps } from 'components/navigation/Header/Header';
 import { FixedFooterLayout } from 'components/structural/FixedFooterLayout';
 import { MainContentLayout } from 'components/structural/MainContentLayout';
 import { RenderSections } from 'components/utils/structural/RenderSections/RenderSections';
 import { Metadata } from 'components/utils/structural/Metadata/Metadata';
 import { HomePage as HomePageProps, GlobalMetadata } from 'types/sanity-schema';
-import { HeaderProps } from 'components/navigation/Header/Header';
 
 export const HomePage = ({
   page,
