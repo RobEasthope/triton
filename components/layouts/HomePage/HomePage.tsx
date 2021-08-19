c8584e4f690e06bd4dc31bdcddc9d167630a440d
Rob Easthope <hello@robeasthope.com>
2021-08-19 17:13:50 +0100

    Initial work on Header integration to homepage

--- components/layouts/HomePage/HomePage.tsx
+++ components/layouts/HomePage/HomePage.tsx
@@ -2,6 +2,7 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable react/prop-types */
 import { Footer } from 'components/navigation/Footer/Footer';
+import { Header } from 'components/navigation/Header/Header';
 import { FixedFooterLayout } from 'components/structural/FixedFooterLayout';
 import { MainContentLayout } from 'components/structural/MainContentLayout';
 import { RenderSections } from 'components/utils/RenderSections/RenderSections';
@@ -9,6 +10,16 @@ import { RenderSections } from 'components/utils/RenderSections/RenderSections';
 export const HomePage = ({ page, preview }) => (
   <>
     <FixedFooterLayout>
+      {Object.keys(globals?.header).length > 0 && (
+        <>
+          <Header
+            logo={globals?.header?.logo}
+            navigation={globals?.header?.navigation}
+            preview={preview}
+          />
+        </>
+      )}
+
       <MainContentLayout as="main">
         {page?.sections && (
           <RenderSections sections={page?.sections} preview={preview} />
