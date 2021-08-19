08e90609dcf18eb29b9d7559057bc8e54d13ddf6
Rob Easthope <hello@robeasthope.com>
2021-08-19 17:05:43 +0100

    Work on Page props

--- components/layouts/Page/Page.tsx
+++ components/layouts/Page/Page.tsx
@@ -2,18 +2,24 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable react/prop-types */
 import { Footer } from 'components/navigation/Footer/Footer';
+import { HeaderProps } from 'components/navigation/Header/Header';
 import { FixedFooterLayout } from 'components/structural/FixedFooterLayout';
 import { MainContentLayout } from 'components/structural/MainContentLayout';
+import { Metadata } from 'components/utils/structural/Metadata/Metadata';
 import { RenderSections } from 'components/utils/structural/RenderSections/RenderSections';
+import { Page as PageProps, GlobalMetadata } from 'types/sanity-schema';
 
 export const Page = ({
   page,
+  globals,
   preview,
 }: {
-  page: Record<string, unknown>;
+  page: PageProps;
+  globals: { header: HeaderProps; metadata: GlobalMetadata };
   preview: boolean;
 }) => (
   <>
+    <Metadata page={page} globalMetadata={globals?.metadata} />
     <FixedFooterLayout>
       <MainContentLayout as="main">
         {page?.sections && (
