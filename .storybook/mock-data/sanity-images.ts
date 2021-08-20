d677b6d91fdf93c4129fe4092d17ac5fddfc9361
Rob Easthope <hello@robeasthope.com>
2021-08-20 11:32:14 +0100

    Add Red arrows mock data

--- .storybook/mock-data/sanity-images.ts
+++ .storybook/mock-data/sanity-images.ts
@@ -13,4 +13,28 @@ export const TRIQUETRA_LOGO = {
       _ref: 'image-4954fadfe0d52dac93b575b16c3f6892f54add7e-512x480-svg',
       _type: 'reference',
     },
-  } 
+  }
+
+export const RED_ARROWS = {
+  "_type": "image",
+  "asset": {
+    "_ref": "image-ea1262b2a7e68d46710e342c0c83df4fcfc81eb4-6000x4000-jpg",
+    "_type": "reference"
+  },
+  "attribution": "Read arrows roaring over Horseguards parade. Summer 2020",
+  "caption": "Red Arrows roaring over Horseguards parade",
+  "crop": {
+    "_type": "sanity.imageCrop",
+    "bottom": 0,
+    "left": 0,
+    "right": 0.1797945205479452,
+    "top": 0
+  },
+  "hotspot": {
+    "_type": "sanity.imageHotspot",
+    "height": 0.5502077419885639,
+    "width": 0.7123287671232875,
+    "x": 0.3664383561643833,
+    "y": 0.27510387099428196
+  }
+}
