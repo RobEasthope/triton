298b074c0d3aee1d22aa6668a972e1bf2a980bf8
Rob Easthope <hello@robeasthope.com>
2021-08-20 16:05:54 +0100

    Test RawHeading in sandbox

--- components/utils/Sandbox/Sandbox.tsx
+++ components/utils/Sandbox/Sandbox.tsx
@@ -2,10 +2,14 @@ import faker from 'faker';
 import { ExampleText } from 'components/utils/formatted-text/ExampleText/ExampleText';
 import { EXAMPLE_TEXT } from '.storybook/mock-data/sanity-text';
 import { capitalizeFirstLetter } from '.storybook/utils/capitalizeFirstLetter';
+import { RawHeading } from '../text/RawHeading/RawHeading';
 
 export const Sandbox = () => (
   <section>
-    <h1>{capitalizeFirstLetter(faker.lorem.words())}</h1>
+    <RawHeading as="h1" min={18} max={32} my={18}>
+      {capitalizeFirstLetter(faker.lorem.words())}
+    </RawHeading>
+
     <ExampleText blocks={EXAMPLE_TEXT} preview={false} />
   </section>
 );
