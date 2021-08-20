d1fb3d9b7bbc430a6f26f7c37939c6d3fcbf5195
Rob Easthope <hello@robeasthope.com>
2021-08-20 12:56:17 +0100

    Add example

--- components/typography/Heading/Heading.tsx
+++ components/typography/Heading/Heading.tsx
@@ -1,8 +1,9 @@
 import { Heading as ChakraHeading } from '@chakra-ui/react';
 import styled from '@emotion/styled';
+import { type } from 'utils/responsive/type';
 
 export const Heading = styled(ChakraHeading)<{
   as: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
 }>`
-  font-size: clamp(1rem, 12rem, 12rem);
+  ${type({ min: 16, max: 32 })}
 `;
