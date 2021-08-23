949ba59e76b4abc9795f2f97b6e4956f1224459c
Rob Easthope <hello@robeasthope.com>
2021-08-23 10:02:03 +0100

    Add AppDispatch typing

--- redux/store.ts
+++ redux/store.ts
@@ -1,4 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { useDispatch } from 'react-redux';
 import mobileNavReducer from 'redux/slices/mobileNavSlice';
 
 const store = configureStore({
@@ -6,6 +7,9 @@ const store = configureStore({
     mobileNav: mobileNavReducer,
   },
 });
+
+export type AppDispatch = typeof store.dispatch;
+export const useAppDispatch = () => useDispatch<AppDispatch>();
 
 export type RootState = ReturnType<typeof store.getState>;
 export default store;
