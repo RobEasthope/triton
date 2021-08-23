f9a9ea51d922274aa1bc67fa95753816aa214470
Rob Easthope <hello@robeasthope.com>
2021-08-23 11:17:31 +0100

    Add mobile nav open button

--- components/navigation/Header/Header.tsx
+++ components/navigation/Header/Header.tsx
@@ -8,6 +8,9 @@ import {
   InternalLinkWithTitleProp,
 } from 'types/links';
 import styled from '@emotion/styled';
+import { Button } from '@chakra-ui/react';
+import { openMobileNav } from 'redux/slices/mobileNavSlice';
+import { useDispatch } from 'react-redux';
 import { SuperLink } from '../raw-links/SuperLink/SuperLink';
 import { MobileNav } from '../MobileNav/MobileNav';
 
@@ -26,32 +29,43 @@ export const Header = ({
   logo,
   navigation,
   preview,
-}: Pick<HeaderProps, 'logo' | 'navigation' | 'preview'>) => (
-  <>
-    <header>
-      <div>
-        <HeaderLogo>
-          <Picture
-            asset={logo as ImageAssetProp}
-            mode="contain"
-            maxWidth={32}
-            preview={preview}
-          />
-        </HeaderLogo>
+}: Pick<HeaderProps, 'logo' | 'navigation' | 'preview'>) => {
+  const dispatch = useDispatch();
+
+  return (
+    <>
+      <header>
         <div>
-          <ul>
-            {navigation?.length > 0 &&
-              navigation.map((nav) => (
-                <li key={nav?._key} className="link">
-                  <SuperLink className="" link={nav}>
-                    {nav.title}
-                  </SuperLink>
-                </li>
-              ))}
-          </ul>
+          <HeaderLogo>
+            <Picture
+              asset={logo as ImageAssetProp}
+              mode="contain"
+              maxWidth={32}
+              preview={preview}
+            />
+          </HeaderLogo>
+          <div>
+            <ul>
+              {navigation?.length > 0 &&
+                navigation.map((nav) => (
+                  <li key={nav?._key} className="link">
+                    <SuperLink className="" link={nav}>
+                      {nav.title}
+                    </SuperLink>
+                  </li>
+                ))}
+            </ul>
+          </div>
+          <button
+            type="button"
+            aria-label="Increment value"
+            onClick={() => dispatch(openMobileNav())}
+          >
+            Nav
+          </button>
         </div>
-      </div>
-    </header>
-    <MobileNav navigation={navigation} />
-  </>
-);
+      </header>
+      <MobileNav navigation={navigation} />
+    </>
+  );
+};
