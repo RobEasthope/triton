d8727603aeec32be98ee43ac6864493205b84a29
Rob Easthope <hello@robeasthope.com>
2021-08-23 11:18:35 +0100

    Restore DrawerCloseButton

--- components/navigation/MobileNav/MobileNav.tsx
+++ components/navigation/MobileNav/MobileNav.tsx
@@ -34,7 +34,7 @@ export const MobileNav = ({
     >
       <DrawerOverlay />
       <DrawerContent>
-        {/* <DrawerCloseButton /> */}
+        <DrawerCloseButton />
 
         <DrawerBody>
           <nav>
