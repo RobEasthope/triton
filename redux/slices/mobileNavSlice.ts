d7c87693450600aa0752f1d1a2ebb5d707e30237
Rob Easthope <hello@robeasthope.com>
2021-08-23 09:13:43 +0100

    Draft out mobileNavSlice

---
+++ redux/slices/mobileNavSlice.ts
@@ -0,0 +1,28 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  mobileNavOpen: false,
+};
+
+export const mobileNavSlice = createSlice({
+  name: 'mobileNav',
+  initialState,
+  reducers: {
+    openMobileNav: (state) => {
+      state.mobileNavOpen = true;
+    },
+    closeMobileNav: (state) => {
+      state.mobileNavOpen = false;
+    },
+    toggleMobileNav: (state) => {
+      state.mobileNavOpen != state.mobileNavOpen;
+    },
+  },
+});
+
+export const { openMobileNav, closeMobileNav, toggleMobileNav } =
+  mobileNavSlice.actions;
+
+export const selectMobileNavStatus = (state) => state.mobileNav.mobileNavOpen;
+
+export default mobileNavSlice.reducer;
