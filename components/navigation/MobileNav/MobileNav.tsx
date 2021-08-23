a838d28453c5e36edf14465832fcdd28335c4f36
Rob Easthope <hello@robeasthope.com>
2021-08-23 11:06:17 +0100

    Draft out chakra UI drawer integration

--- components/navigation/MobileNav/MobileNav.tsx
+++ components/navigation/MobileNav/MobileNav.tsx
@@ -3,6 +3,16 @@ import {
   ExternalLinkWithTitleProp,
   InternalLinkWithTitleProp,
 } from 'types/links';
+import {
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerOverlay,
+  useDisclosure,
+} from '@chakra-ui/react';
+import { useSelector } from 'react-redux';
+import { selectMobileNavStatus } from 'redux/slices/mobileNavSlice';
 import { SuperLink } from '../raw-links/SuperLink/SuperLink';
 
 export interface MobileNavProps extends rawHeaderProps {
@@ -11,17 +21,36 @@ export interface MobileNavProps extends rawHeaderProps {
 
 export const MobileNav = ({
   navigation,
-}: Pick<MobileNavProps, 'navigation'>) => (
-  <nav>
-    <ul>
-      {navigation?.length > 0 &&
-        navigation.map((nav) => (
-          <li key={nav?._key} className="link">
-            <SuperLink className="" link={nav}>
-              {nav.title}
-            </SuperLink>
-          </li>
-        ))}
-    </ul>
-  </nav>
-);
+}: Pick<MobileNavProps, 'navigation'>) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const mobileNavOpen = useSelector(selectMobileNavStatus);
+
+  return (
+    <Drawer
+      isOpen={mobileNavOpen}
+      placement="left"
+      onClose={onClose}
+      // finalFocusRef={btnRef}
+    >
+      <DrawerOverlay />
+      <DrawerContent>
+        {/* <DrawerCloseButton /> */}
+
+        <DrawerBody>
+          <nav>
+            <ul>
+              {navigation?.length > 0 &&
+                navigation.map((nav) => (
+                  <li key={nav?._key} className="link">
+                    <SuperLink className="" link={nav}>
+                      {nav.title}
+                    </SuperLink>
+                  </li>
+                ))}
+            </ul>
+          </nav>
+        </DrawerBody>
+      </DrawerContent>
+    </Drawer>
+  );
+};
