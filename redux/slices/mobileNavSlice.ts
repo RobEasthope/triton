c57dd7b020acba5923da5b54614aec3e42a871d3
Rob Easthope <hello@robeasthope.com>
2021-08-23 10:06:53 +0100

    Creat MobileNavSliceProps

--- redux/slices/mobileNavSlice.ts
+++ redux/slices/mobileNavSlice.ts
@@ -1,6 +1,10 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const initialState = {
+type MobileNavSliceProps = {
+  mobileNavOpen: boolean;
+};
+
+const initialState: MobileNavSliceProps = {
   mobileNavOpen: false,
 };
 
