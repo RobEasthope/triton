0a6cb233ee582a6aabceea9139a9dcfe941c3b28
Rob Easthope <hello@robeasthope.com>
2021-08-23 10:43:19 +0100

    Remove AppDispatch code

--- redux/store.ts
+++ redux/store.ts
@@ -1,5 +1,4 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { useDispatch } from 'react-redux';
 import mobileNavReducer from 'redux/slices/mobileNavSlice';
 
 const store = configureStore({
@@ -7,9 +6,6 @@ const store = configureStore({
     mobileNav: mobileNavReducer,
   },
 });
-
-export type AppDispatch = typeof store.dispatch;
-export const useAppDispatch = () => useDispatch<AppDispatch>();
 
 export type RootState = ReturnType<typeof store.getState>;
 export default store;
