f65e6e3fd56085370c2513e2ab6caa72f9d60028
Rob Easthope <hello@robeasthope.com>
2021-08-23 11:15:05 +0100

    Move MobileNav to Header

--- components/navigation/Header/Header.tsx
+++ components/navigation/Header/Header.tsx
@@ -9,6 +9,7 @@ import {
 } from 'types/links';
 import styled from '@emotion/styled';
 import { SuperLink } from '../raw-links/SuperLink/SuperLink';
+import { MobileNav } from '../MobileNav/MobileNav';
 
 export interface HeaderProps extends rawHeaderProps {
   navigation?: [ExternalLinkWithTitleProp, InternalLinkWithTitleProp];
@@ -26,28 +27,31 @@ export const Header = ({
   navigation,
   preview,
 }: Pick<HeaderProps, 'logo' | 'navigation' | 'preview'>) => (
-  <header>
-    <div>
-      <HeaderLogo>
-        <Picture
-          asset={logo as ImageAssetProp}
-          mode="contain"
-          maxWidth={32}
-          preview={preview}
-        />
-      </HeaderLogo>
+  <>
+    <header>
       <div>
-        <ul>
-          {navigation?.length > 0 &&
-            navigation.map((nav) => (
-              <li key={nav?._key} className="link">
-                <SuperLink className="" link={nav}>
-                  {nav.title}
-                </SuperLink>
-              </li>
-            ))}
-        </ul>
+        <HeaderLogo>
+          <Picture
+            asset={logo as ImageAssetProp}
+            mode="contain"
+            maxWidth={32}
+            preview={preview}
+          />
+        </HeaderLogo>
+        <div>
+          <ul>
+            {navigation?.length > 0 &&
+              navigation.map((nav) => (
+                <li key={nav?._key} className="link">
+                  <SuperLink className="" link={nav}>
+                    {nav.title}
+                  </SuperLink>
+                </li>
+              ))}
+          </ul>
+        </div>
       </div>
-    </div>
-  </header>
+    </header>
+    <MobileNav navigation={navigation} />
+  </>
 );
