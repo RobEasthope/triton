a78aef51ede1db0b5125dc81def98c4aecd8f455
Rob Easthope <hello@robeasthope.com>
2021-08-24 12:01:42 +0100

    Rename schema files

--- .sanity-studio/schemas/schema-index.ts
+++ .sanity-studio/schemas/schema-index.ts
@@ -5,12 +5,12 @@ import createSchema from 'part:@sanity/base/schema-creator';
 import schemaTypes from 'all:part:@sanity/base/schema-type';
 
 // Navigation
-import internalLink from '../../components/navigation/raw-links/InternalLink/internal-link-schema.js';
-import internalLinkWithTitle from '../../components/navigation/raw-links/InternalLink/internal-link-with-title-schema.js';
-import externalLink from '../../components/navigation/raw-links/ExternalLink/external-link-schema.js';
-import externalLinkWithTitle from '../../components/navigation/raw-links/ExternalLink/external-link-with-title-schema.js';
+import internalLink from '../../components/navigation/raw-links/InternalLink/InternalLink.schema.js';
+import internalLinkWithTitle from '../../components/navigation/raw-links/InternalLink/InternalLinkWithTitle.schema.js';
+import externalLink from '../../components/navigation/raw-links/ExternalLink/ExternalLink.schema.js';
+import externalLinkWithTitle from '../../components/navigation/raw-links/ExternalLink/ExternalLinkWithTitle.schema.js';
 
-import header from '../../components/navigation/Header/header-schema.js';
+import header from '../../components/navigation/Header/Header.schema.js';
 
 // Settings
 import globalMetadata from '../../components/utils/structural/Metadata/global-metadata-schema';
@@ -19,11 +19,11 @@ import globalMetadata from '../../components/utils/structural/Metadata/global-me
 import exampleText from '../../components/utils/formatted-text/ExampleText/example-text-schema';
 
 // Sections
-import exampleSection from '../../components/sections/ExampleSection/example-section-schema';
+import exampleSection from '../../components/sections/ExampleSection/ExampleSection.schema';
 
 // Pages
-import homePage from '../../components/layouts/HomePage/home-page-schema';
-import page from '../../components/layouts/Page/page-schema';
+import homePage from '../../components/layouts/HomePage/HomePage.schema';
+import page from '../../components/layouts/Page/Page.schema';
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
