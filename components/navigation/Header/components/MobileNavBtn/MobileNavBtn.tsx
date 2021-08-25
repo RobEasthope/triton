25ce1078ef18aa44e41bca32f4c6e743455c9e51
Rob Easthope <hello@robeasthope.com>
2021-08-25 17:47:28 +0100

    Remove all Redux code

--- components/navigation/Header/components/MobileNavBtn/MobileNavBtn.tsx
+++ components/navigation/Header/components/MobileNavBtn/MobileNavBtn.tsx
@@ -1,22 +1,16 @@
-import { useDispatch } from 'react-redux';
 import { RiMenuLine } from 'react-icons/ri';
-import { openMobileNav } from 'components/navigation/MobileNav/mobileNav.slice';
 
 type MobileNavBtnProps = {
   className?: string;
 };
 
-export const MobileNavBtn = ({ className, onClick }: MobileNavBtnProps) => {
-  const dispatch = useDispatch();
-
-  return (
-    <button
-      type="button"
-      aria-label="Open mobile navigation"
-      onClick={() => onClick()}
-      className={className}
-    >
-      <RiMenuLine />
-    </button>
-  );
-};
+export const MobileNavBtn = ({ className, onClick }: MobileNavBtnProps) => (
+  <button
+    type="button"
+    aria-label="Open mobile navigation"
+    onClick={() => onClick()}
+    className={className}
+  >
+    <RiMenuLine />
+  </button>
+);
--- components/navigation/MobileNav/MobileNav.tsx
+++ components/navigation/MobileNav/MobileNav.tsx
@@ -8,13 +8,8 @@ import {
   DrawerBody,
   DrawerContent,
   DrawerOverlay,
-  useDisclosure,
 } from '@chakra-ui/react';
-import { useDispatch, useSelector } from 'react-redux';
-import {
-  closeMobileNav,
-  selectMobileNavStatus,
-} from 'components/navigation/MobileNav/mobileNav.slice';
+
 import { RiCloseLine } from 'react-icons/ri';
 import { SuperLink } from '../../utils/links/SuperLink/SuperLink';
 
@@ -26,44 +21,39 @@ export const MobileNav = ({
   navigation,
   isOpen,
   onClose,
-}: Pick<MobileNavProps, 'navigation'>) => {
-  const dispatch = useDispatch();
-  const mobileNavOpen = useSelector(selectMobileNavStatus);
+}: Pick<MobileNavProps, 'navigation'>) => (
+  <Drawer
+    isOpen={isOpen}
+    placement="left"
+    onClose={onClose}
+    closeOnOverlayClick
+    closeOnEsc
+    // finalFocusRef={btnRef}
+  >
+    <DrawerOverlay />
+    <DrawerContent>
+      <button
+        type="button"
+        aria-label="Increment value"
+        onClick={() => onClose()}
+      >
+        <RiCloseLine />
+      </button>
 
-  return (
-    <Drawer
-      isOpen={isOpen}
-      placement="left"
-      onClose={onClose}
-      closeOnOverlayClick
-      closeOnEsc
-      // finalFocusRef={btnRef}
-    >
-      <DrawerOverlay />
-      <DrawerContent>
-        <button
-          type="button"
-          aria-label="Increment value"
-          onClick={() => dispatch(closeMobileNav())}
-        >
-          <RiCloseLine />
-        </button>
-
-        <DrawerBody>
-          <nav>
-            <ul>
-              {navigation?.length > 0 &&
-                navigation.map((nav) => (
-                  <li key={nav?._key} className="link">
-                    <SuperLink className="" link={nav}>
-                      {nav.title}
-                    </SuperLink>
-                  </li>
-                ))}
-            </ul>
-          </nav>
-        </DrawerBody>
-      </DrawerContent>
-    </Drawer>
-  );
-};
+      <DrawerBody>
+        <nav>
+          <ul>
+            {navigation?.length > 0 &&
+              navigation.map((nav) => (
+                <li key={nav?._key} className="link">
+                  <SuperLink className="" link={nav}>
+                    {nav.title}
+                  </SuperLink>
+                </li>
+              ))}
+          </ul>
+        </nav>
+      </DrawerBody>
+    </DrawerContent>
+  </Drawer>
+);
