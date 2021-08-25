d93e08994850b36b13180e5048960cde79bcd903
Rob Easthope <hello@robeasthope.com>
2021-08-25 17:40:08 +0100

    Explicitly set Drawer options

--- components/navigation/MobileNav/MobileNav.tsx
+++ components/navigation/MobileNav/MobileNav.tsx
@@ -34,6 +34,8 @@ export const MobileNav = ({
       isOpen={mobileNavOpen}
       placement="left"
       onClose={onClose}
+      closeOnOverlayClick
+      closeOnEsc
       // finalFocusRef={btnRef}
     >
       <DrawerOverlay />
