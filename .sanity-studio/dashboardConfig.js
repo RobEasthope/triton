4858b357556fcb695814fa0ffbfea21d055579df
Rob Easthope <hello@robeasthope.com>
2021-08-27 11:59:10 +0100

    Add project_info and project_users widgets

--- .sanity-studio/dashboardConfig.js
+++ .sanity-studio/dashboardConfig.js
@@ -1,11 +1,16 @@
 export default {
   widgets: [
-    // ...
     {
       name: 'vercel',
       layout: {
         width: 'full', // full width is recommended!
       },
     },
+    {
+      name: 'project-info'
+    },
+    {
+      name: 'project-users'
+    }
   ],
 }
