f17577ac5588e16787d9eed9317ef7b497aaf8a3
Rob Easthope <hello@robeasthope.com>
2021-08-31 17:25:02 +0100

    Import stitches in _app instead of _document to follow new next linting rules

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,19 @@ import { ReactNode } from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
 import { theme } from 'settings/theme/theme';
 import { globalStyles } from 'settings/styles/globalStyles';
+import Head from 'next/head';
+import { getCssText } from 'stitches.config';
 
 function MyApp({ Component, pageProps }: AppProps): ReactNode {
   return (
     <ChakraProvider theme={theme}>
       {globalStyles}
+      <Head>
+        <style
+          id="stitches"
+          dangerouslySetInnerHTML={{ __html: getCssText() }}
+        />
+      </Head>
       <Component {...pageProps} />
     </ChakraProvider>
   );
