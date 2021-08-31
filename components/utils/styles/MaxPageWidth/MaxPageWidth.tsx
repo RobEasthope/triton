a1cb760b05b799835fa02084a336b584e00bc53c
Rob Easthope <hello@robeasthope.com>
2021-08-31 22:41:56 +0100

    Fix MaxPageWidth centring

--- components/utils/styles/MaxPageWidth/MaxPageWidth.tsx
+++ components/utils/styles/MaxPageWidth/MaxPageWidth.tsx
@@ -2,5 +2,6 @@ import { styled } from 'stitches.config';
 
 export const MaxPageWidth = styled('div', {
   maxWidth: '1200px',
-  marginX: 'auto',
+  marginLeft: 'auto',
+  marginRight: 'auto',
 });
