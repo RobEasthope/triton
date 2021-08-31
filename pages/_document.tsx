d2b5e45fa3cbf96bd3aeb6006e2ba1a5c0b00abf
Rob Easthope <hello@robeasthope.com>
2021-08-31 17:33:12 +0100

    Switch back to _document after correctly reading the docs...

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,33 @@
-import Document, { DocumentContext } from 'next/document';
+import Document, {
+  DocumentContext,
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from 'next/document';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx);
 
     return initialProps;
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <style
+            id="stitches"
+            dangerouslySetInnerHTML={{ __html: getCssText() }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
 
