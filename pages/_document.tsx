68e1e09f954f8efe1c8edd71e54542632514eea3
Rob Easthope <hello@robeasthope.com>
2021-08-31 17:44:10 +0100

    Import getCssText

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,7 @@ import Document, {
   Main,
   NextScript,
 } from 'next/document';
+import { getCssText } from 'stitches.config';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
