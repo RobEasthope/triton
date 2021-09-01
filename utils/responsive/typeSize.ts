1b4e34500d08dde34baf5b914d38f0cf58c2b8cb
Rob Easthope <hello@robeasthope.com>
2021-09-01 15:46:42 +0100

    Viably set font-size responsively via stitches

--- utils/responsive/typeSize.ts
+++ utils/responsive/typeSize.ts
@@ -3,11 +3,16 @@ export const typeSize = ({ min, max }: { min: number; max: number }) => {
   const maxWidth = 1200;
   const unit = 'px';
   return {
-    fontSize: `${min}px`,
+    fontSize: `${min}${unit}`,
 
     '@bp1': {
       // fontSize: `${`calc(${min}${unit} + (${max} - ${min}) * ((100vw - ${minWidth}${unit}) / (${maxWidth} - ${minWidth})))`}`,
-      fontSize: `calc(${max}${unit})`,
+      fontSize: `calc(${min}${unit} + (${max} - ${min}) * ((100vw - ${minWidth}${unit}) / (${maxWidth} - ${minWidth})))`,
+      // fontSize: 'calc(32px + (64 - 32) * ((100vw - 320px) / (1400 - 320)))',
+    },
+
+    '@bp2': {
+      fontSize: `${max}${unit}`,
     },
   };
 };
