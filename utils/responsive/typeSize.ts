87a605fa49378df320e65523c08cddaae54906c7
Rob Easthope <hello@robeasthope.com>
2021-09-01 17:06:49 +0100

    Add remaining styles

--- utils/responsive/typeSize.ts
+++ utils/responsive/typeSize.ts
@@ -1,19 +1,47 @@
 type TypeSizeProps = {
+  // Font sizes
   min: number;
   max: number;
+
+  // Margin
   mt?: number;
+  mr?: number;
   mb?: number;
   ml?: number;
-  mr?: number;
+  mx?: number;
+  my?: number;
+
+  // Padding
+  pt?: number;
+  pr?: number;
+  pb?: number;
+  pl?: number;
+  px?: number;
+  py?: number;
 };
 
-export const typeSize = ({ min, max, mt, mb, ml, mr }: TypeSizeProps) => {
+export const typeSize = ({
+  min,
+  max,
+  mt,
+  mr,
+  mb,
+  ml,
+  mx,
+  my,
+  pt,
+  pr,
+  pb,
+  pl,
+  px,
+  py,
+}: TypeSizeProps) => {
   const minWidth = 320;
   const maxWidth = 1200;
   const unit = 'px';
 
   return {
-    // Font sizing
+    // Font size
     fontSize: `${min}${unit}`,
     '@bp1': {
       fontSize: `calc(${min}${unit} + (${max} - ${min}) * ((100vw - ${minWidth}${unit}) / (${maxWidth} - ${minWidth})))`,
@@ -22,10 +50,20 @@ export const typeSize = ({ min, max, mt, mb, ml, mr }: TypeSizeProps) => {
       fontSize: `${max}${unit}`,
     },
 
-    // Margins
+    // Margin
     marginTop: `${mt ? `${mt / max}em` : null}`,
     marginBottom: `${mb ? `${mb / max}em` : null}`,
     marginLeft: `${ml ? `${ml / max}em` : null}`,
     marginRight: `${mr ? `${mr / max}em` : null}`,
+    marginX: `${mx ? `${mx / max}em` : null}`,
+    marginY: `${my ? `${my / max}em` : null}`,
+
+    // Padding
+    paddingTop: `${pt ? `${pt / max}em` : null}`,
+    paddingBottom: `${pb ? `${pb / max}em` : null}`,
+    paddingLeft: `${pl ? `${pl / max}em` : null}`,
+    paddingRight: `${pr ? `${pr / max}em` : null}`,
+    paddingX: `${px ? `${px / max}em` : null}`,
+    paddingY: `${py ? `${py / max}em` : null}`,
   };
 };
