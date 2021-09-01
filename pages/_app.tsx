67778dd36f18cc57d2681392fe69b83e0917f39a
Rob Easthope <hello@robeasthope.com>
2021-09-01 22:09:15 +0100

    Remove emotion based global styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,10 @@ import type { AppProps } from 'next/app';
 import { ReactNode } from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
 import { theme } from 'settings/theme/theme';
-import { globalStyles } from 'settings/styles/globalStyles';
 
 function MyApp({ Component, pageProps }: AppProps): ReactNode {
   return (
     <ChakraProvider theme={theme}>
-      {globalStyles}
       <Component {...pageProps} />
     </ChakraProvider>
   );
