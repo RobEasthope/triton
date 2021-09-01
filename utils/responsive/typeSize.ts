8b310f315ccf85cfb663541d54b815aea81d28d2
Rob Easthope <hello@robeasthope.com>
2021-09-01 15:47:39 +0100

    Remove old code

--- utils/responsive/typeSize.ts
+++ utils/responsive/typeSize.ts
@@ -6,9 +6,7 @@ export const typeSize = ({ min, max }: { min: number; max: number }) => {
     fontSize: `${min}${unit}`,
 
     '@bp1': {
-      // fontSize: `${`calc(${min}${unit} + (${max} - ${min}) * ((100vw - ${minWidth}${unit}) / (${maxWidth} - ${minWidth})))`}`,
       fontSize: `calc(${min}${unit} + (${max} - ${min}) * ((100vw - ${minWidth}${unit}) / (${maxWidth} - ${minWidth})))`,
-      // fontSize: 'calc(32px + (64 - 32) * ((100vw - 320px) / (1400 - 320)))',
     },
 
     '@bp2': {
