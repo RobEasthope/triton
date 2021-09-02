9e9ab15e87d53d158e9040019dcebf2a213c74b9
Rob Easthope <hello@robeasthope.com>
2021-09-02 14:36:13 +0100

    Remove Chakra UI element and replace with Radix UI dialog

--- components/navigation/Header/Header.tsx
+++ components/navigation/Header/Header.tsx
@@ -5,7 +5,6 @@ import {
   InternalLinkWithTitleProp,
 } from 'types/links';
 import { MaxPageWidth } from 'components/utils/styles/MaxPageWidth/MaxPageWidth';
-import { useDisclosure } from '@chakra-ui/react';
 import { RiMenuLine } from 'react-icons/ri';
 import { PaddedComponent } from 'components/utils/styles/PaddedComponent/PaddedComponent';
 import { SuperLink } from '../../utils/links/SuperLink/SuperLink';
@@ -26,39 +25,26 @@ export const Header = ({
   logo,
   navigation,
   preview,
-}: Pick<HeaderProps, 'logo' | 'navigation' | 'preview'>) => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
+}: Pick<HeaderProps, 'logo' | 'navigation' | 'preview'>) => (
+  <>
+    <PaddedComponent as="header">
+      <MaxPageWidth as="nav">
+        <HeaderLayout>
+          <HeaderLogo logo={logo} preview={preview} />
 
-  return (
-    <>
-      <PaddedComponent as="header">
-        <MaxPageWidth as="nav">
-          <HeaderLayout>
-            <HeaderLogo logo={logo} preview={preview} />
+          <LargeNavigation className="navigation">
+            {navigation?.length > 0 &&
+              navigation.map((nav) => (
+                <li key={nav?._key}>
+                  <SuperLink link={nav}>{nav.title}</SuperLink>
+                </li>
+              ))}
+          </LargeNavigation>
 
-            <LargeNavigation className="navigation">
-              {navigation?.length > 0 &&
-                navigation.map((nav) => (
-                  <li key={nav?._key}>
-                    <SuperLink link={nav}>{nav.title}</SuperLink>
-                  </li>
-                ))}
-            </LargeNavigation>
-
-            <MobileNavButton
-              type="button"
-              aria-label="Open mobile navigation"
-              onClick={onOpen}
-              className="mobile-nav-btn"
-            >
-              <RiMenuLine />
-            </MobileNavButton>
-          </HeaderLayout>
-        </MaxPageWidth>
-      </PaddedComponent>
-
-      {/* Small Nav */}
-      <MobileNav navigation={navigation} isOpen={isOpen} onClose={onClose} />
-    </>
-  );
-};
+          {/* Small Nav */}
+          <MobileNav navigation={navigation} />
+        </HeaderLayout>
+      </MaxPageWidth>
+    </PaddedComponent>
+  </>
+);
--- components/navigation/MobileNav/MobileNav.tsx
+++ components/navigation/MobileNav/MobileNav.tsx
@@ -3,53 +3,42 @@ import {
   ExternalLinkWithTitleProp,
   InternalLinkWithTitleProp,
 } from 'types/links';
-import {
-  Drawer,
-  DrawerBody,
-  DrawerContent,
-  DrawerOverlay,
-} from '@chakra-ui/react';
 
-import { RiCloseLine } from 'react-icons/ri';
+import { RiCloseLine, RiMenuLine } from 'react-icons/ri';
+import * as DialogPrimative from '@radix-ui/react-dialog';
 import { SuperLink } from '../../utils/links/SuperLink/SuperLink';
 
 export interface MobileNavProps extends rawHeaderProps {
   navigation?: [ExternalLinkWithTitleProp, InternalLinkWithTitleProp];
-  isOpen: boolean;
-  onClose: VoidFunction;
 }
 
 export const MobileNav = ({
   navigation,
-  isOpen,
-  onClose,
-}: Pick<MobileNavProps, 'navigation' | 'isOpen' | 'onClose'>) => (
-  <Drawer
-    isOpen={isOpen}
-    placement="left"
-    onClose={onClose}
-    // finalFocusRef={btnRef}
-  >
-    <DrawerOverlay />
-    <DrawerContent>
-      <button type="button" aria-label="Increment value" onClick={onClose}>
-        <RiCloseLine />
-      </button>
+}: Pick<MobileNavProps, 'navigation'>) => (
+  <DialogPrimative.Root>
+    <DialogPrimative.Trigger>
+      <RiMenuLine />
+    </DialogPrimative.Trigger>
+    <DialogPrimative.Overlay />
+    <DialogPrimative.Content>
+      <DialogPrimative.Title />
+      <DialogPrimative.Description />
 
-      <DrawerBody>
-        <nav>
-          <ul>
-            {navigation?.length > 0 &&
-              navigation.map((nav) => (
-                <li key={nav?._key} className="link">
-                  <SuperLink className="" link={nav}>
-                    {nav.title}
-                  </SuperLink>
-                </li>
-              ))}
-          </ul>
-        </nav>
-      </DrawerBody>
-    </DrawerContent>
-  </Drawer>
+      <DialogPrimative.Close>
+        <RiCloseLine />
+      </DialogPrimative.Close>
+      <nav>
+        <ul>
+          {navigation?.length > 0 &&
+            navigation.map((nav) => (
+              <li key={nav?._key} className="link">
+                <SuperLink className="" link={nav}>
+                  {nav.title}
+                </SuperLink>
+              </li>
+            ))}
+        </ul>
+      </nav>
+    </DialogPrimative.Content>
+  </DialogPrimative.Root>
 );
