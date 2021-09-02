6b450a6044f13e8fdecfe0a6e150a28e8c2222b5
Rob Easthope <hello@robeasthope.com>
2021-09-02 15:16:33 +0100

    Creat globalStyles with sanitze.css imports

---
+++ settings/styles/globalStyles.tsx
@@ -0,0 +1,9 @@
+import { globalCss } from 'stitches.config';
+import 'sanitize.css';
+import 'sanitize.css/typography.css';
+import 'sanitize.css/assets.css';
+import 'sanitize.css/forms.css';
+import 'sanitize.css/forms.evergreen.css';
+import 'sanitize.css/reduce-motion.css';
+
+export const globalStyles = globalCss({});
