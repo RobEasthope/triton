ec68dbd1389ef5cae7fa2e0cc9e5546236f0c836
Rob Easthope <hello@robeasthope.com>
2021-09-02 15:16:56 +0100

    Integrate globalStyles into _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,11 @@
 import type { AppProps } from 'next/app';
 import { ReactNode } from 'react';
+import { globalStyles } from 'settings/styles/globalStyles';
 
 function MyApp({ Component, pageProps }: AppProps): ReactNode {
   return (
     <>
+      {globalStyles()}
       <Component {...pageProps} />
     </>
   );
