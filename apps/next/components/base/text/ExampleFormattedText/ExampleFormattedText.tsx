e9b5f210d03703ca90b2f25fe63bfea1185d6656
Rob Easthope <hello@robeasthope.com>
2021-10-05 12:01:20 +0100

    Upgrade ExampleFormattedText

--- apps/next/components/base/text/ExampleFormattedText/ExampleFormattedText.tsx
+++ apps/next/components/base/text/ExampleFormattedText/ExampleFormattedText.tsx
@@ -3,28 +3,32 @@
 /* eslint-disable @typescript-eslint/no-unsafe-return */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
+import { ReactNode } from 'react';
 import BlockContent from '@sanity/block-content-to-react';
-
-// import { YoutubeBlock } from 'components/blocks/YoutubeBlock/YoutubeBlock';
 
 export interface ExampleFormattedTextProps {
   blocks: unknown;
-  className?: string;
-  as?: string;
   preview: boolean;
 }
 
-const BlockRenderer = (props) => {
-  const { style = 'normal' } = props?.node as Record<string, unknown>;
+const BlockRenderer = (props: {
+  node: { style: string };
+  children: ReactNode;
+}) => {
+  // eslint-disable-next-line react/destructuring-assignment
+  const { style = 'normal' } = props?.node;
 
   if (style === 'h2') {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, react/prop-types
     return <h2>{props.children}</h2>;
   }
   if (style === 'h3') {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, react/prop-types
     return <h3>{props.children}</h3>;
   }
 
   if (style === 'h4') {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, react/prop-types
     return <h4>{props.children}</h4>;
   }
 
@@ -40,16 +44,7 @@ const serializers = {
 
 export const ExampleFormattedText = ({
   blocks,
-  className,
-  as,
   preview,
 }: ExampleFormattedTextProps) => (
-  <div className={className}>
-    <BlockContent
-      as={as}
-      blocks={blocks}
-      serializers={serializers}
-      preview={preview}
-    />
-  </div>
+  <BlockContent blocks={blocks} serializers={serializers} preview={preview} />
 );
