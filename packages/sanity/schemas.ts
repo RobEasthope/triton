c83806dbbc93f905c59d6160acb35a29956b3afb
Rob Easthope <hello@robeasthope.com>
2021-10-05 10:17:44 +0100

    Update schema index

--- packages/sanity/schemas.ts
+++ packages/sanity/schemas.ts
@@ -4,26 +4,30 @@ import createSchema from 'part:@sanity/base/schema-creator';
 // Then import schema types from any plugins that might expose them
 import schemaTypes from 'all:part:@sanity/base/schema-type';
 
+//Links
+import internalLink from '../next/components/utils/links/InternalLink/InternalLink.schema.js';
+import internalLinkWithTitle from '../next/components/utils/links/InternalLink/InternalLinkWithTitle.schema.js';
+import externalLink from '../next/components/utils/links/ExternalLink/ExternalLink.schema.js';
+import externalLinkWithTitle from '../next/components/utils/links/ExternalLink/ExternalLinkWithTitle.schema.js';
+
 // Navigation
-import internalLink from '../../components/utils/links/InternalLink/InternalLink.schema.js';
-import internalLinkWithTitle from '../../components/utils/links/InternalLink/InternalLinkWithTitle.schema.js';
-import externalLink from '../../components/utils/links/ExternalLink/ExternalLink.schema.js';
-import externalLinkWithTitle from '../../components/utils/links/ExternalLink/ExternalLinkWithTitle.schema.js';
 
-import header from '../../components/navigation/Header/Header.schema.js';
-
-// Settings
-import globalMetadata from '../../components/utils/rendering/Metadata/GlobalMetadata.schema';
+import header from '../next/components/navigation/Header/Header.schema.js';
 
 // Formatted text
-import exampleText from '../../components/utils/text/ExampleFormattedText/ExampleFormattedText.schema';
+import exampleText from '../next/components/utils/text/ExampleFormattedText/ExampleFormattedText.schema';
 
-// Sections
-import exampleSection from '../../components/sections/ExampleSection/ExampleSection.schema';
+// Settings
+import globalMetadata from '../next/components/utils/rendering/Metadata/GlobalMetadata.schema';
+
+
+
+// Content
+import exampleSection from '../next/components/sections/ExampleSection/ExampleSection.schema';
 
 // Pages
-import homePage from '../../components/layouts/HomePage/HomePage.schema';
-import page from '../../components/layouts/Page/Page.schema';
+import homePage from '../next/components/layouts/HomePage/HomePage.schema';
+import page from '../next/components/layouts/Page/Page.schema';
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -32,21 +36,22 @@ export default createSchema({
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
-    // Navigation
+    // Links
     internalLink,
     internalLinkWithTitle,
     externalLink,
     externalLinkWithTitle,
 
+    // Navigation
     header,
-
-    // Settings
-    globalMetadata,
 
     // Formatted text
     exampleText,
 
-    // Sections
+    // Settings
+    globalMetadata,
+
+    // Content
     exampleSection,
 
     // Pages
