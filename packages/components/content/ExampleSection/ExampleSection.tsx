ec2798515792fc22d4952c48d809dac0a26eddf9
Rob Easthope <hello@robeasthope.com>
2021-10-25 17:48:26 +0100

    Successfully test Picture integration

--- packages/components/content/ExampleSection/ExampleSection.tsx
+++ packages/components/content/ExampleSection/ExampleSection.tsx
@@ -3,6 +3,7 @@ import { MaxPageWidth } from '@/components/base/styles/MaxPageWidth/MaxPageWidth
 import { ExampleSection as rawExampleSectionProps } from '@/components/types/sanity-schema';
 import { PaddedComponent } from '@/components/base/styles/PaddedComponent/PaddedComponent';
 import { Heading } from './ExampleSection.styles';
+import { Picture } from '@/components/base/media/Picture/Picture';
 
 export interface ExampleSectionProps extends rawExampleSectionProps {
   preview: boolean;
@@ -11,6 +12,7 @@ export interface ExampleSectionProps extends rawExampleSectionProps {
 export const ExampleSection = ({
   heading,
   text,
+  image,
   preview,
 }: ExampleSectionProps) => {
   if (!heading && !text) {
@@ -22,6 +24,7 @@ export const ExampleSection = ({
       <MaxPageWidth>
         {heading && <Heading as="h1">{heading}</Heading>}
         {text && <ExampleFormattedText blocks={text} preview={preview} />}
+        {text && <Picture asset={image} mode="responsive" maxWidth={800} preview={preview} />}
       </MaxPageWidth>
     </PaddedComponent>
   );
