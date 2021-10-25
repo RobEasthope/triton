908df1fa4b3f966af5f654e07930224c084fb9d7
Rob Easthope <hello@robeasthope.com>
2021-10-25 15:24:16 +0100

    Move sanitize css imports to _app

--- packages/components/styles/globalStyles.tsx
+++ packages/components/styles/globalStyles.tsx
@@ -1,9 +1,3 @@
-import { globalCss } from 'styles/stitches.config';
-import 'sanitize.css';
-import 'sanitize.css/typography.css';
-import 'sanitize.css/assets.css';
-import 'sanitize.css/forms.css';
-import 'sanitize.css/forms.evergreen.css';
-import 'sanitize.css/reduce-motion.css';
+import { globalCss } from '@/components/styles/stitches.config';
 
 export const globalStyles = globalCss({});
--- packages/next/pages/_app.tsx
+++ packages/next/pages/_app.tsx
@@ -1,6 +1,12 @@
 import type { AppProps } from 'next/app';
 import { ReactNode } from 'react';
 import { globalStyles } from '@/components/styles/globalStyles';
+import 'sanitize.css';
+import 'sanitize.css/typography.css';
+import 'sanitize.css/assets.css';
+import 'sanitize.css/forms.css';
+import 'sanitize.css/forms.evergreen.css';
+import 'sanitize.css/reduce-motion.css';
 
 function MyApp({ Component, pageProps }: AppProps): ReactNode {
   return (
