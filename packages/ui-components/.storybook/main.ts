8af0957483e6f91c29fbc6e30bcf1c110f59798b
Rob Easthope <hello@robeasthope.com>
2021-11-12 13:27:48 +0000

    Get storybook to resolve TS paths

--- packages/ui-components/.storybook/main.ts
+++ packages/ui-components/.storybook/main.ts
@@ -1,4 +1,5 @@
 const path = require('path');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 module.exports = {
   "stories": [
@@ -15,9 +16,18 @@ module.exports = {
     "@whitespace/storybook-addon-html",
     "storybook-addon-code-preview"
   ],
-  webpackFinal: async (config) => {
+  webpackFinal: async (config, { configType }) => {
     config.resolve.modules = [path.resolve(__dirname, ".."), "node_modules"];
-
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname, "../"),
+    };
+    config.resolve.plugins = [
+      ...(config.resolve.plugins || []),
+      new TsconfigPathsPlugin({
+        extensions: config.resolve.extensions,
+      }),
+    ];
     return config;
   },
 }
