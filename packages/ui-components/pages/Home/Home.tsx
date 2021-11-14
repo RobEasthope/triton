ad24e5af2708d9b5e239c0e095976184fdf88cb3
Rob Easthope <hello@robeasthope.com>
2021-11-14 13:14:05 +0000

    Fix paths to MainContentLayout

--- packages/ui-components/pages/Home/Home.tsx
+++ packages/ui-components/pages/Home/Home.tsx
@@ -4,7 +4,7 @@
 import { Footer } from '@/UI/navigation/Footer/Footer';
 import { Header, HeaderProps } from '@/UI/navigation/Header/Header';
 import { FixedFooterLayout } from '@/UI/base/layout/FixedFooterLayout/FixedFooterLayout';
-import { MainContentLayout } from '@/UI/structural/MainContentLayout';
+import { MainContentLayout } from '@/UI/base/layout/MainContentLayout/MainContentLayout';
 import { RenderSections } from '@/UI/base/app/RenderSections/RenderSections';
 import { Metadata } from '@/UI/base/app/Metadata/Metadata';
 import {
