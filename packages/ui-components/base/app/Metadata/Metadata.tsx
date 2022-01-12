3f8f9e1889c8ea4fae9fed111e13276277a054dc
Rob Easthope <hello@robeasthope.com>
2022-01-12 19:19:53 +0000

    Update metadata image generation

--- packages/ui-components/base/app/Metadata/Metadata.tsx
+++ packages/ui-components/base/app/Metadata/Metadata.tsx
@@ -1,59 +1,71 @@
 import Head from 'next/head';
-
-import { urlForImage } from '@/UTILS/sanity/sanity-utils';
+import { SanityImageSource } from '@sanity/image-url/lib/types/types';
+import { createImageUrlBuilder } from 'next-sanity';
 import { GlobalMetadata, Home, Page } from '@/UI/types/sanity-schema';
 import { METADATA } from '@/UI/constants/METADATA';
+import { sanityConfig } from '@/UTILS/sanity/sanity-config';
 
 type MetadateType = {
   page: Page | Home;
   globalMetadata: GlobalMetadata;
 };
 
-export const Metadata = ({ page, globalMetadata }: MetadateType) => (
-  <Head>
-    {/* Standard HTML */}
-    {page?.metadataTitle && (
-      <title>
-        {`${page?.metadataTitle} ${
-          globalMetadata?.globalTitleTemplate || ''
-        }` || METADATA.TITLE}
-      </title>
-    )}
-    {page?.metadataDescription && (
-      <meta name="description" content={page?.metadataDescription} />
-    )}
+export const Metadata = ({ page, globalMetadata }: MetadateType) => {
+  const imageBuilder = createImageUrlBuilder(sanityConfig);
+  const urlForImage = (source: SanityImageSource) =>
+    imageBuilder.image(source).auto('format').fit('max');
+  return (
+    <Head>
+      {/* Standard HTML */}
+      {page?.metadataTitle && (
+        <title>
+          {`${page?.metadataTitle} ${
+            globalMetadata?.globalTitleTemplate || ''
+          }` || METADATA.TITLE}
+        </title>
+      )}
+      {page?.metadataDescription && (
+        <meta name="description" content={page?.metadataDescription} />
+      )}
 
-    {/* Opengraph */}
-    {page?.metadataTitle && (
-      <meta
-        property="og:title"
-        content={`${page?.metadataTitle} ${
-          globalMetadata?.globalTitleTemplate || ''
-        }`}
-      />
-    )}
-    {page?.metadataDescription && (
-      <meta property="og:description" content={page?.metadataDescription} />
-    )}
-    {page?.metadataImage && (
-      <meta
-        property="og:image"
-        content={urlForImage(page?.metadataImage).width(1200).height(630).url()}
-      />
-    )}
+      {/* Opengraph */}
+      {page?.metadataTitle && (
+        <meta
+          property="og:title"
+          content={`${page?.metadataTitle} ${
+            globalMetadata?.globalTitleTemplate || ''
+          }`}
+        />
+      )}
+      {page?.metadataDescription && (
+        <meta property="og:description" content={page?.metadataDescription} />
+      )}
+      {page?.metadataImage && (
+        <meta
+          property="og:image"
+          content={urlForImage(page?.metadataImage)
+            .width(1200)
+            .height(630)
+            .url()}
+        />
+      )}
 
-    {/* Twitter */}
-    {page?.metadataTitle && (
-      <meta name="twitter:title" content={page?.metadataTitle} />
-    )}
-    {page?.metadataDescription && (
-      <meta name="twitter:description" content={page?.metadataDescription} />
-    )}
-    {page?.metadataImage && (
-      <meta
-        name="twitter:image"
-        content={urlForImage(page?.metadataImage).width(1200).height(630).url()}
-      />
-    )}
-  </Head>
-);
+      {/* Twitter */}
+      {page?.metadataTitle && (
+        <meta name="twitter:title" content={page?.metadataTitle} />
+      )}
+      {page?.metadataDescription && (
+        <meta name="twitter:description" content={page?.metadataDescription} />
+      )}
+      {page?.metadataImage && (
+        <meta
+          name="twitter:image"
+          content={urlForImage(page?.metadataImage)
+            .width(1200)
+            .height(630)
+            .url()}
+        />
+      )}
+    </Head>
+  );
+};
