a5efd62c78695066750e03d8a4818ad81debbcf4
Rob Easthope <hello@robeasthope.com>
2022-01-12 16:25:12 +0000

    Update selectSanityQuery types

--- apps/trq-next/sanity-api/selectSanityQuery.ts
+++ apps/trq-next/sanity-api/selectSanityQuery.ts
@@ -1,7 +1,12 @@
 import { anyPageBySlugQuery } from '@/UI/pages/Page/Page.queries';
 import { createSlugFromQuery } from '@/UTILS/sanity/createSlugFromQuery';
 
-export function selectSanityQuery(slugArray = []): Record<string, unknown> {
+type SelectSanityQueryProps = {
+  sanityQuery: string;
+  queryParams: { slug: string };
+};
+
+export function selectSanityQuery(slugArray = []): SelectSanityQueryProps {
   if (slugArray.length === 0) {
     return {
       sanityQuery: anyPageBySlugQuery,
