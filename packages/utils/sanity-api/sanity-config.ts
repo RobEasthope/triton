62042fd3ba02996caf05fce9572533822b97a855
Rob Easthope <hello@robeasthope.com>
2022-01-17 15:16:33 +0000

    Add types for sanityConfig

--- packages/utils/sanity-api/sanity-config.ts
+++ packages/utils/sanity-api/sanity-config.ts
@@ -1,8 +1,17 @@
-export const sanityConfig = {
+type sanityConfigProps = {
+  dataset: string
+  projectId: string
+  apiVersion: string
+  token: string
+  useCdn: boolean,
+  withCredentials: boolean,
+}
+
+export const sanityConfig: sanityConfigProps = {
   dataset: 'production',
   projectId: 'xtxdr2ns',
   apiVersion: '2021-08-19',
   token: process.env.SANITY_API_TOKEN,
   useCdn: false,
   withCredentials: true,
-};
+}
