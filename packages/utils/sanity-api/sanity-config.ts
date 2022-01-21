62f4e4c216b91cfd6c2865ce3b142bd8df64e923
Rob Easthope <hello@robeasthope.com>
2022-01-21 18:37:21 +0000

    Fix formatting

--- packages/utils/sanity-api/sanity-config.ts
+++ packages/utils/sanity-api/sanity-config.ts
@@ -1,17 +1,21 @@
 type sanityConfigProps = {
-  projectId: string,
-  dataset: string,
-  apiVersion: string,
-  token: string,
-  useCdn: boolean,
-  withCredentials: boolean,
-}
+  projectId: string;
+  dataset: string;
+  apiVersion: string;
+  token: string;
+  useCdn: boolean;
+  withCredentials: boolean;
+};
 
 export const sanityConfig: sanityConfigProps = {
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || process.env.STORYBOOK_SANITY_PROJECT_ID,
-  dataset: process.env.NEXT_PUBLIC_SANITY_PROJECT_DATASET || process.env.STORYBOOK_SANITY_DATASET,
+  projectId:
+    process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ||
+    process.env.STORYBOOK_SANITY_PROJECT_ID,
+  dataset:
+    process.env.NEXT_PUBLIC_SANITY_PROJECT_DATASET ||
+    process.env.STORYBOOK_SANITY_DATASET,
   apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION,
   token: process.env.SANITY_API_TOKEN,
   useCdn: false,
   withCredentials: true,
-}
+};
