51964e7062e00aaf638d9b494e6ca14055295b1b
Rob Easthope <hello@robeasthope.com>
2022-01-21 10:00:12 +0000

    Import sanitize.css/system-ui.css

--- apps/trq-next/pages/_app.tsx
+++ apps/trq-next/pages/_app.tsx
@@ -7,6 +7,7 @@ import 'sanitize.css';
 import 'sanitize.css/typography.css';
 import 'sanitize.css/assets.css';
 import 'sanitize.css/forms.css';
+import 'sanitize.css/system-ui.css';
 import 'sanitize.css/reduce-motion.css';
 
 function MyApp({ Component, pageProps }: AppProps): ReactNode {
