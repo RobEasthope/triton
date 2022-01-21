9bd2614c5fdd3f8df20a3c427e3cbff848a1e34a
Rob Easthope <hello@robeasthope.com>
2022-01-21 09:58:46 +0000

    Remove sanitize.css/forms.evergreen.css import

--- apps/trq-next/pages/_app.tsx
+++ apps/trq-next/pages/_app.tsx
@@ -7,7 +7,6 @@ import 'sanitize.css';
 import 'sanitize.css/typography.css';
 import 'sanitize.css/assets.css';
 import 'sanitize.css/forms.css';
-import 'sanitize.css/forms.evergreen.css';
 import 'sanitize.css/reduce-motion.css';
 
 function MyApp({ Component, pageProps }: AppProps): ReactNode {
