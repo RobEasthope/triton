587e53e06fbb4e463f2fd0bd6f119755afb1addd
Rob Easthope <hello@robeasthope.com>
2022-01-23 22:35:21 +0000

    Fix TS issues in sanityConfig

--- packages/utils/sanity-api/sanity-config.ts
+++ packages/utils/sanity-api/sanity-config.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 type SanityConfigProps = {
   projectId: string;
   dataset: string;
@@ -9,13 +10,13 @@ type SanityConfigProps = {
 
 export const sanityConfig: SanityConfigProps = {
   projectId:
-    process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ||
-    process.env.STORYBOOK_SANITY_PROJECT_ID,
+    process.env.NEXT_PUBLIC_SANITY_PROJECT_ID! ||
+    process.env.STORYBOOK_SANITY_PROJECT_ID!,
   dataset:
-    process.env.NEXT_PUBLIC_SANITY_PROJECT_DATASET ||
-    process.env.STORYBOOK_SANITY_DATASET,
-  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION,
-  token: process.env.SANITY_API_TOKEN,
+    process.env.NEXT_PUBLIC_SANITY_PROJECT_DATASET! ||
+    process.env.STORYBOOK_SANITY_DATASET!,
+  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION!,
+  token: process.env.SANITY_API_TOKEN!,
   useCdn: false,
   withCredentials: true,
 };
