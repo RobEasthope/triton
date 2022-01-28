fbc5fc49df2f997ce48d8727390e6e0fb0794615
Rob Easthope <hello@robeasthope.com>
2022-01-28 17:17:14 +0000

    Create Settings.schema.js

--- packages/ui/base/settings/Settings.schema.js
+++ packages/ui/base/settings/Settings.schema.js
@@ -1,12 +1,10 @@
 import { ALL_PAGES_TYPES } from '../../utils/sanity/ALL_PAGE_TYPES';
-import { headingUI } from '../../utils/sanity/headingUI';
 
 export default {
   name: 'Settings',
   type: 'document',
   title: 'Site settings',
   fields: [
-    headingUI('Settings'),
     {
       name: 'rawHomePageRef',
       title: 'Home page',
@@ -15,64 +13,6 @@ export default {
       description: 'Select home page (Critical requirement)',
       codegen: { required: true },
       validation: (Rule) => Rule.required(),
-    },
-    headingUI('SEO & metadata'),
-    {
-      name: 'globalSitename',
-      title: 'Sitename',
-      type: 'string',
-      description: 'Used for SEO & descriptive purposes',
-      codegen: { required: true },
-      validation: (Rule) => Rule.required(),
-    },
-    {
-      name: 'globalTitle',
-      title: 'Page title',
-      type: 'string',
-      description:
-        'Can be overridden on a page by page basis in the pages "Metadata" tab.',
-      codegen: { required: true },
-      validation: (Rule) => Rule.required(),
-    },
-    {
-      name: 'globalTitleTemplate',
-      title: 'Page title template',
-      type: 'string',
-      description: 'Displayed after the page title e.g. PAGE TITLE | Ida',
-      codegen: { required: true },
-      validation: (Rule) => Rule.required(),
-    },
-    {
-      name: 'globalDescription',
-      title: 'Page description',
-      type: 'text',
-      description:
-        'Can be overridden on a page by page basis in the pages "Metadata" tab.',
-      codegen: { required: true },
-      validation: (Rule) => Rule.required(),
-    },
-    {
-      name: 'globalThumbnail',
-      title: 'Page thumbnail',
-      type: 'image',
-      description:
-        'Can be overridden on a page by page basis in the pages "Metadata" tab.',
-      codegen: { required: true },
-      validation: (Rule) => Rule.required(),
-    },
-    {
-      name: 'canonicalUrl',
-      title: 'Canonical URL',
-      type: 'url',
-      description: 'The main site url. Used to create canonical url',
-    },
-    {
-      name: 'globalTwitterSiteHandle',
-      title: 'Twitter site username',
-      type: 'string',
-      description:
-        'https://developer.twitter.com/en/docs/twitter-for-websites/cards/guides/getting-started#attribution',
-      validation: (Rule) => Rule.required().warning(),
     },
   ],
   preview: {
